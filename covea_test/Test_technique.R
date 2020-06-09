f295585dff0874808c396939fa61fe1573b7d237
Thi Bao Châu NGUYEN <nguyen.tbc812@gmail.com>
2020-06-09 14:11:47 +0200

    last commit

--- covea_test/Test_technique.R
+++
@@ -1,4 +0,0 @@
-# Read des données #
-url = "https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20200416-115822/valeursfoncieres-2019.txt"
-df = read.csv(url,sep='|',header = TRUE)
-#?re
